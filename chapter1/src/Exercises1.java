652e147639794908144c2ab11952999c990cfbfb
Bethel Melesse Tessema <28558632+bettymelesse@users.noreply.github.com>
2021-07-16 04:30:08 +0900

    adding chapter one
    Signed-off-by: Bethel Melesse Tessema <28558632+bettymelesse@users.noreply.github.com>

---
+++ chapter1/src/Exercises1.java
@@ -0,0 +1,6 @@
+public class Exercises1 {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+        System.out.println("How are you?"); //but we already know the world is dying
+    }
+}
